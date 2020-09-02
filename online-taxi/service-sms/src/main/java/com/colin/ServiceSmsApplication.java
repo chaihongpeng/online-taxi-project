185740b14fe433a67669e2d1f42d3b96142e2775
chaihongpeng <chai_hongpeng@163.com>
2020-09-02 17:26:01 +0800

    1.1

--- online-taxi/service-sms/src/main/java/com/colin/ServiceSmsApplication.java
+++ online-taxi/service-sms/src/main/java/com/colin/ServiceSmsApplication.java
@@ -3,9 +3,9 @@ package com.colin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PassengerWebApplication {
+// @SpringBootApplication
+public class ServiceSmsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PassengerWebApplication.class, args);
+        // SpringApplication.run(ServiceSmsApplication.class, args);
     }
 }
