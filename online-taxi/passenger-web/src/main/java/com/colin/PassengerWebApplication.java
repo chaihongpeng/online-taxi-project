772cfd020b7fb76e90530fdf66feb34bd0bdcfc0
chaihongpeng <chai_hongpeng@163.com>
2020-09-02 16:54:26 +0800

    1.1

---
+++ online-taxi/passenger-web/src/main/java/com/colin/PassengerWebApplication.java
@@ -0,0 +1,11 @@
+package com.colin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerWebApplication.class, args);
+    }
+}
